0d135ffce816414bd6031a80b6b5815dbbbc4720
Calle Sjöberg <callesjoberg@MacBook-Pro-som-tillhor-Calle.local>
2024-04-22 11:47:35 -0400

    greetings.sh

---
+++ Assignment7/greetings.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Prompt the user for their name
+echo "Hello! What's your name?"
+
+# Read the user's input into a variable
+read name
+
+# Greet the user
+echo "Hello, $name! Nice to meet you."
